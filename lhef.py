87a5f949160ab10cebb51ed0797252192caf322b
Roberto <foice.news@gmail.com>
2018-05-27 17:09:26 +0200

    first commit

---
+++ lhef.py
@@ -0,0 +1,73 @@
+import os
+
+class LHEFile(object):
+    def __init__(self):
+        pass
+
+class LHEEvent(object):
+    def __init__(self,eventinfo,particles):
+        self.eventinfo = eventinfo
+        self.particles = particles
+        for p in self.particles:
+            p.event = self
+
+class LHEEventInfo(object):
+    fieldnames = ['nparticles', 'pid', 'weight', 'scale', 'aqed', 'aqcd']
+    def __init__(self, **kwargs):
+        if not set(kwargs.keys()) == set(self.fieldnames):
+            raise RuntimeError
+        for k,v in kwargs.items():
+            setattr(self,k,v)
+
+    @classmethod
+    def fromstring(cls,string):
+        return cls(**dict(list(zip(cls.fieldnames,list(map(float,string.split()))))))
+
+
+import lorentz
+
+
+class LHEParticle(object):
+    fieldnames = fieldnames = ['id','status','mother1','mother2','color1','color2','px','py','pz','e','m','lifetime','spin']
+    def __init__(self, **kwargs):
+        if not set(kwargs.keys()) == set(self.fieldnames):
+            raise RuntimeError
+        for k,v in kwargs.items():
+            setattr(self,k,v)
+
+    @classmethod
+    def fromstring(cls,string):
+        obj = cls(**dict(list(zip(cls.fieldnames,list(map(float,string.split()))))))
+        return obj
+
+    def mothers(self):
+        mothers = []
+        first_idx  =  int(self.mother1)-1
+        second_idx =  int(self.mother2)-1
+        for idx in set([first_idx,second_idx]):
+            if idx >= 0: mothers.append(self.event.particles[idx])
+        return mothers
+
+    def fourvector(self):
+        lv=lorentz.LorentzVector(self.px, self.py, self.pz, self.e)
+        return lv
+
+def loads():
+    pass
+
+import xml.etree.ElementTree as ET
+def readLHE(thefile):
+    try:
+        for event,element in ET.iterparse(thefile,events=['end']):
+            if element.tag == 'event':
+                data = element.text.split('\n')[1:-1]
+                eventdata,particles = data[0],data[1:]
+                eventinfo = LHEEventInfo.fromstring(eventdata)
+                particle_objs = []
+                for p in particles:
+                    particle_objs+=[LHEParticle.fromstring(p)]
+                yield LHEEvent(eventinfo,particle_objs)
+
+    except ET.ParseError:
+        print("WARNING. Parse Error.")
+        return
---
+++ lorentz.py
@@ -0,0 +1,53 @@
+# with contributions from https://github.com/lukasheinrich/lorentz/blob/master/lorentz/
+
+import math
+
+METRIC = [-1,-1,-1,1]
+
+def contract_tuples(lhs,rhs,metric = None):
+    return sum(m*l*r for m,l,r in zip(metric if metric else [1]*len(lhs),lhs,rhs))
+
+class LorentzVector(object):
+
+    """
+    A class for Lorentz four-vectors stuff
+    """
+    def __init__(self,px,py,pz,e):
+        self._px = px
+        self._py = py
+        self._pz = pz
+        self._e = e
+
+    def components(self):
+        return (self.px,self.py,self.pz,self.e)
+
+    # https://www.programiz.com/python-programming/property
+
+    @property
+    def px(self):
+        return self._px
+    @property
+    def py(self):
+        return self._py
+    @property
+    def pz(self):
+        return self._pz
+    @property
+    def e(self):
+        return self._e
+
+    # @property
+    # def theta_from_pT(self):
+    #     return math.atan2(self.perp(), self.e)
+    @property
+    def theta(self):
+        return math.acos(self.pz / math.sqrt(self.px**2 + self.py**2 + self.pz**2 ) )
+
+    def cosTheta(self):
+        return math.cos(self.theta)
+
+    def perp2(self):
+        transvers_comps = self.components()[0:1]
+        return contract_tuples(transvers_comps,transvers_comps)
+    def perp(self):
+        return math.sqrt(self.perp2())
