c940f457fa85fdb9e7081fa4b12b271ec4b27a07
franceschini.roberto@gmail.com <foice.news@gmail.com>
2018-08-14 16:42:13 +0200

    analysis_result_to_pandas

--- analyses.py
+++ analyses.py
@@ -11,3 +11,10 @@ class AnalysisResult(object):
     def __init__(self,histograms,cuts_matrix):
         self.histograms = histograms
         self.cuts_matrix = cuts_matrix
+
+
+
+def analysis_result_to_pandas(analysis_result=None, names=None):
+    _D=[analysis_result.histograms.list_of_histograms[name].exclusive for name in names ]
+    _pd_observables=observables.concatenate_results(_D,columns=names)
+    return _pd_observables
