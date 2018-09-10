113d4ba0119e5db23c1f3896665cb74da3a618cf
franceschini.roberto@gmail.com <foice.news@gmail.com>
2018-09-10 17:43:51 +0200

    reverted to exclusive-only concatenation

--- analyses.py
+++ analyses.py
@@ -1,14 +1,5 @@
 import pandas as pd
 
-def concatenate_results(list_of_jsons=None,columns=None):
-    if columns is not None:
-        _w=pd.DataFrame( list_of_jsons[0] ).set_index('event_number')['weight']
-        _p=pd.concat(map(lambda x: pd.DataFrame(x).set_index('event_number')['values'], list_of_jsons ),axis=1, sort=False)
-        _p.columns = columns
-        _p=pd.concat([_p,_w],axis=1, sort=False)
-    return _p
-
-
 class AnalysisResult(object):
     """
 
@@ -24,12 +15,20 @@ class AnalysisResult(object):
         self.cuts_matrix = cuts_matrix
 
 
+def concatenate_results(list_of_jsons=None,columns=None):
+    if columns is not None:
+        _w=pd.DataFrame( list_of_jsons[0] ).set_index('event_number')['weight'] # DataFrame with the Weight
+        _p=pd.concat(map(lambda x: pd.DataFrame(x).set_index('event_number')['values'], list_of_jsons ),axis=1, sort=False)
+        _p.columns = columns
+        _p=pd.concat([_p,_w],axis=1, sort=False)
+    return _p
+
 
 def analysis_result_to_pandas(analysis_result=None, names=None):
-    _Dexclusive=[analysis_result.histograms.list_of_histograms[name].exclusive for name in names ]
-    _Dinclusive=[analysis_result.histograms.list_of_histograms[name].inclusive for name in names ]
-    _D=_Dexclusive+_Dinclusive
-    _names_inclusive = [ nam+'_inclusive' for nam in names ]
-    _names = names +_names_inclusive
-    _pd_observables=concatenate_results(_D,columns=_names)
+    _Dexclusive=[analysis_result.histograms.list_of_histograms[name].exclusive for name in names ] #list [list of JSON ]
+    #_Dinclusive=[analysis_result.histograms.list_of_histograms[name].inclusive for name in names ]
+    _D= _Dexclusive #_Dinclusive #_Dexclusive#+_Dinclusive
+    #_names_inclusive = [ nam+'_inclusive' for nam in names ]
+    _names = names #+_names_inclusive
+    _pd_observables=concatenate_results(list_of_jsons=_D,columns=_names)
     return _pd_observables
