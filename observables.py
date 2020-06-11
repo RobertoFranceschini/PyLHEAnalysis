79a895abea18b346e47749e9ee68e2073c348b16
RobertoFranceschini <franceschini.roberto@gmail.com>
2020-06-11 19:32:26 +0200

    properties

--- observables.py
+++ observables.py
@@ -174,19 +174,19 @@ def px(lhe_particles):
     _lv = lorentz.LorentzVector()
     for lv in lhe_particles:
         _lv=lv.fourvector()+_lv
-    return _lv.px()
+    return _lv.px
 
 def py(lhe_particles):
     _lv = lorentz.LorentzVector()
     for lv in lhe_particles:
         _lv=lv.fourvector()+_lv
-    return _lv.py()
+    return _lv.py
 
 def pz(lhe_particles):
     _lv = lorentz.LorentzVector()
     for lv in lhe_particles:
         _lv=lv.fourvector()+_lv
-    return _lv.pz()
+    return _lv.pz
 
 def pLong(lhe_particles):
     _lv = lorentz.LorentzVector()
