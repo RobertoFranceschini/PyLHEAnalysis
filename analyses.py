b3ca099c92cda32ade362d2a1154620dc63f842f
RobertoFranceschini <franceschini.roberto@gmail.com>
2020-07-28 16:44:34 +0200

    tagged the slow part

--- analyses.py
+++ analyses.py
@@ -33,10 +33,12 @@ def concatenate_results(list_of_jsons=None,columns=None):
         start_time = time.time()
         _w=pd.DataFrame( list_of_jsons[0] ).set_index('event_number')['weight'] # DataFrame with the Weight
         logger.info("--- %s seconds ---" % (time.time() - start_time))
+        ####
         logger.info("Making _p=pd.concat(map(lambda x: pd.DataFrame(x).set_index('event_number')['values'], list_of_jsons ),axis=1, sort=False)")
-        start_time = time.time()
+        start_time = time.time() # THIS THE SLOW PART
         _p=pd.concat(map(lambda x: pd.DataFrame(x).set_index('event_number')['values'], list_of_jsons ),axis=1, sort=False)
         logger.info("--- %s seconds ---" % (time.time() - start_time))
+        ####
         _p.columns = columns
         logger.info("Making _p=pd.concat([_p,_w],axis=1, sort=False)")
         start_time = time.time()
